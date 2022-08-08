7137e5c856b8dab0d53f46acfc0a80c239055fcd
Victor Barroncas <victor@boostsecurity.io>
2022-08-08 16:46:32 -0400

    vadec

--- env.py
+++ env.py
@@ -16,7 +16,7 @@ def generateJWT2(userId):
     timeLimit = datetime.datetime.utcnow(
     ) + datetime.timedelta(minutes=30)  # set limit for user
     payload = {"user_id": userId, "exp": timeLimit}
-    return jwt.encode(payload, "super-secret-6FDFBB8F-2909-4565-85EA-3F685784355E")
+    return jwt.encode(payload, "super-secret-6FDFBB8F-2909-4565-85EA-3F685784355A")
 
 
 def generateJWT3(userId):
@@ -24,4 +24,4 @@ def generateJWT3(userId):
     timeLimit = datetime.datetime.utcnow(
     ) + datetime.timedelta(minutes=30)  # set limit for user
     payload = {"user_id": userId, "exp": timeLimit}
-    return jwt.encode(payload, "super-secret-6FDFBB8F-2909-4565-85EA-3F685784355E")
+    return jwt.encode(payload, "super-secret-6FDFBB8F-2909-4565-85EA-3F685784355B")
