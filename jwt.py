f097103904ee7753a0571002c7cb06c5741f62e1
Victor Barroncas <victor@boostsecurity.io>
2022-09-29 23:40:57 -0400

    fix suppressed

--- jwt.py
+++ jwt.py
@@ -2,12 +2,11 @@ import datetime
 
 import jwt
 
+# def generateJWT(userId):
 
-def generateJWT(userId):
-
-    # Generate token
-    timeLimit = datetime.datetime.utcnow() + datetime.timedelta(
-        minutes=30
-    )  # set limit for user
-    payload = {"user_id": userId, "exp": timeLimit}
-    return jwt.encode(payload, "super-secret-6FDFBB8F--3F685784355E")  # noboost
+#     # Generate token
+#     timeLimit = datetime.datetime.utcnow() + datetime.timedelta(
+#         minutes=30
+#     )  # set limit for user
+#     payload = {"user_id": userId, "exp": timeLimit}
+#     return jwt.encode(payload, "super-secret-6FDFBB8F--3F685784355E")  # noboost
