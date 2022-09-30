3007b46df64df37b2594008cd397ea2199d06e98
Victor Barroncas <victor@boostsecurity.io>
2022-09-29 23:30:38 -0400

    add violation

--- jwt.py
+++ jwt.py
@@ -2,10 +2,12 @@ import datetime
 
 import jwt
 
-# def generateJWT(userId):
 
-#     #Generate token
-#     timeLimit = datetime.datetime.utcnow(
-#     ) + datetime.timedelta(minutes=30)  # set limit for user
-#     payload = {"user_id": userId, "exp": timeLimit}
-#     return jwt.encode(payload, "super-secret-6FDFBB8F-2909-4565-85EA-3F685784355E")
+def generateJWT(userId):
+
+    # Generate token
+    timeLimit = datetime.datetime.utcnow() + datetime.timedelta(
+        minutes=30
+    )  # set limit for user
+    payload = {"user_id": userId, "exp": timeLimit}
+    return jwt.encode(payload, "super-secret-6FDFBB8F-2909-4565-85EA-3F685784355E")
