0897e6d8d63551647bb721fefe5e7d678ca24dc1
victor-nb <59754066+victor-nb@users.noreply.github.com>
2022-11-08 18:33:39 -0400

    Update jwt.py

--- jwt.py
+++ jwt.py
@@ -21,3 +21,12 @@ def generateJWT2(userId):
     )  # set limit for user
     payload = {"user_id": userId, "exp": timeLimit}
     return jwt.encode(payload, "super-secret-6FDFBB8F--3F685784355E")
+
+def generateJWT3(userId):
+
+    # Generate token
+    timeLimit = datetime.datetime.utcnow() + datetime.timedelta(
+        minutes=30
+    )  # set limit for user
+    payload = {"user_id": userId, "exp": timeLimit}
+    return jwt.encode(payload, "super-secret-6FDFBB8F--3F685784355E")
